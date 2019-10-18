05a169be95e068b02a5ff8f0a1e11269c2dacf65
Piyush <piyushnariani09@gmail.com>
2019-10-18 16:45:07 -0400

    Initial Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const video = document.getElementById('video');
+
+Promise.all([
+	faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+	faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+	faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+	faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+	])
+	.then(startVideo())
+	.catch(err => console.log(err));
+	
+
+function startVideo() {
+	navigator.getUserMedia({video: true}, stream => video.srcObject = stream, err => console.log(err));
+}
+
+video.addEventListener('play', () => {
+	const canvas = faceapi.createCanvasFromMedia(video);
+	document.querySelector('#face-api').append(canvas)
+	const displaySize = {width: 720, height: 560}
+	faceapi.matchDimensions(canvas, displaySize);
+	setInterval(async () => {
+		const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks()
+		// console.log(detections);
+		const resizedDetections = faceapi.resizeResults (detections, displaySize);
+		canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+		faceapi.draw.drawDetections(canvas, resizedDetections);
+		faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+		// faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+		// video.style.display = 'none';
+		}, 100)
+})
