499a193350bbea2a3a73251ca27eefc45c077793
Piyush <piyushnariani09@gmail.com>
2019-10-21 11:11:19 -0400

    Update app.js

--- app.js
+++ app.js
@@ -30,9 +30,9 @@ video.addEventListener('play', () => {
 			drawBox.draw(canvas);
 		})
 		canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-		faceapi.draw.drawDetections(canvas, resizedDetections);
-		faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
-		faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+		// faceapi.draw.drawDetections(canvas, resizedDetections);
+		// faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+		// faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
 		// video.style.display = 'none';
 		}, 100)
 })
