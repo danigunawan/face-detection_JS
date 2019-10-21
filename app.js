c3fbd4dd835931d2f3b0a872ca4fb31a9efced7e
Piyush <piyushnariani09@gmail.com>
2019-10-21 10:50:49 -0400

    Calling face descriptors

--- app.js
+++ app.js
@@ -21,8 +21,7 @@ video.addEventListener('play', () => {
 	const displaySize = {width: 720, height: 560}
 	faceapi.matchDimensions(canvas, displaySize);
 	setInterval(async () => {
-		const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
-		// console.log(detections);
+		const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions().withFaceDescriptors();
 		const resizedDetections = faceapi.resizeResults (detections, displaySize);
 		canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
 		faceapi.draw.drawDetections(canvas, resizedDetections);
